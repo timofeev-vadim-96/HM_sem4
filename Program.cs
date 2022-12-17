2bbcba4dc41eff473474f6f57b6738371f0aae9e
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-17 13:47:09 +0300

    task 25

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
-﻿// Метод, который принимает на вход число и выдает сумму цифр в числе.
-System.Console.WriteLine("Введите число");
-int numb = Convert.ToInt32(Console.ReadLine());
-
-int NumbersSum (int tithing)
+﻿// Цикл, который принимает на вход два числ (А и В) и возводит число А в натуральную степень В.
+System.Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+int Exponentiation (int FirstNumb, int SecondNumb)
 {
-    int sum = 0;
-    if (tithing < 0) tithing = tithing * -1;
-    while (tithing >= 10)
+    int product = 1;
+    for (int i = 0; i < SecondNumb; i++)
     {
-        sum = sum + tithing % 10;
-        tithing = tithing / 10;
+        product = product*FirstNumb;
     }
-    sum = sum + tithing; 
-    return sum; 
+    System.Console.WriteLine($"Число {A} в степени {B} = {product}");
+    return product; 
 }
-NumbersSum(numb);
-System.Console.WriteLine($"Сумма цифр в числе {numb} = {NumbersSum(numb)}");
+Exponentiation(A, B);
+// double sum = Math.Pow(A,B);
+// System.Console.WriteLine(sum);
