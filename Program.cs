e9a4ae55281c1def861c535d76e973ee8a2797c4
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-17 12:40:13 +0300

    task 29

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая задает массив из 8 элементов и выводит их на экран.
+int [] array = new int[8];
+void CreateArray (int [] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = new Random().Next(1,10);
+}
+}   
+void PrintArray (int [] array)
+{
+    System.Console.Write($"[{array[0]},");
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (i == array.Length-1)
+        {
+            System.Console.Write($"{array[i]}]");
+        }
+        else System.Console.Write($"{array[i]},");
+        
+    }
+    System.Console.WriteLine();
+}
+CreateArray(array);
+PrintArray(array);
