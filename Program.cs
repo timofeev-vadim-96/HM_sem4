9c8b3cfb7079ece0e761eba2fbdfbf39d7618956
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-17 14:46:16 +0300

    task 25 with 0 and -B

--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
 ﻿// Цикл, который принимает на вход два числ (А и В) и возводит число А в натуральную степень В.
+// Учитаны случаи ввода 0 и отрицательного значения числ В
 System.Console.WriteLine("Введите первое число");
-int A = Convert.ToInt32(Console.ReadLine());
+double A = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите второе число");
-int B = Convert.ToInt32(Console.ReadLine());
-int Exponentiation (int FirstNumb, int SecondNumb)
+double B = Convert.ToInt32(Console.ReadLine());
+double Exponentiation (double FirstNumb, double SecondNumb)
 {
-    int product = 1;
-    for (int i = 0; i < SecondNumb; i++)
+    double product = 1;
+    if (SecondNumb == 0) product = 1;
+    else
     {
-        product = product*FirstNumb;
+        if (SecondNumb > 0)
+        {
+            for (int i = 0; i < SecondNumb; i++)
+            {
+                product = product * FirstNumb;
+            }
+        }
+        else
+        {
+            for (int i = 0; i > SecondNumb; i--)
+            {
+                product = product * (1/FirstNumb);
+            }
+        }
     }
-    System.Console.WriteLine($"Число {A} в степени {B} = {product}");
-    return product; 
+System.Console.WriteLine($"Число {A} в степени {B} = {product}");
+return product; 
 }
 Exponentiation(A, B);
